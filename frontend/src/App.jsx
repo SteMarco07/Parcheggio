1bfd60db9f9f049fe05407c33b242d316d5256aa
PaoloRobolini <nonilmiopsn@gmail.com>
2026-02-18 11:38:33 +0100

    inizio struttura grafica generale frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import { MapContainer, TileLayer, useMapEvents } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import { useState } from 'react'
-
+import Navbar from './Navbar.jsx';
 
 
 function App() {
 
   const [position, setPosition] = useState(JSON.parse(localStorage.getItem('lastClick'))
-   || [45.55584514965588, 10.216172766008182])
+    || [45.55584514965588, 10.216172766008182])
   const [zoom, setZoom] = useState(JSON.parse(localStorage.getItem('lastZoom')) || 18)
   function ClickLogger() {
     useMapEvents({
@@ -22,16 +22,36 @@ function App() {
   }
 
 
+
   return (
-    <div style={{ margin: '0 auto', marginTop: '20px', marginBottom: '20px', width: '1200px', height: '90vh' }}>
-      <MapContainer center={position} zoom={zoom} style={{ height: '100%', width: '100%' }}>
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        />
-        <ClickLogger />
-      </MapContainer>
-    </div>
+    <>
+      <Navbar />
+      <div className="join join-horizontal gap-5">
+        <div className = "join join-item" style={{ margin: '0 auto', marginTop: '20px', marginBottom: '20px', width: '1200px', height: '90vh' }}>
+          <MapContainer center={position} zoom={zoom} style={{ height: '100%', width: '100%', zIndex: 0 }}>
+            <TileLayer
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            />
+            <ClickLogger />
+          </MapContainer>
+        </div>
+        <div className="join join-item gap-4">
+          <h1>
+            P
+          </h1> 
+          <h2>
+            Parcheggio
+          </h2>
+          <div>
+            {
+                  
+            }
+          </div>
+        </div>
+      </div>
+
+    </>
   );
 }
 
