3d09b86f9e34fe7d4f1951feeb9097885e3ddb75
SteMarco07 <marcoste2007@gmail.com>
2026-02-22 10:47:35 +0100

    Sistemazione responsività pagina auth

--- frontend/src/pages/PaginaAutenticazione.jsx
+++ frontend/src/pages/PaginaAutenticazione.jsx
@@ -10,7 +10,16 @@ function PaginaAutenticazione() {
 
     return (
         <>
-        {mode == "login" ? <LoginForm /> : <SignupForm />}
+            < div className="h-[calc(100vh-64px)] w-full px-6 py-7 flex justify-center" >
+                <div className="w-[90%] lg:w-[30%] lg:min-w-120 md:w-[50%] h-auto bg-white rounded-xl shadow-lg border border-gray-200 p-6 flex flex-col items-center gap-4" >
+                    <div className="flex gap-4">
+                        <button className={`btn ${mode === 'login' ? 'btn-primary' : 'btn-ghost'}`} onClick={() => setMode('login')}>Login</button>
+                        <button className={`btn ${mode === 'signup' ? 'btn-primary' : 'btn-ghost'}`} onClick={() => setMode('signup')}>Signup</button>
+                    </div>
+                    {mode == "login" ? <LoginForm /> : <SignupForm />}
+                </div>
+            </div>
+
         </>
     );
 }
