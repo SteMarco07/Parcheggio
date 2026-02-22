b405730474d0a5289193f05b1ad9468314b1a242
SteMarco07 <marcoste2007@gmail.com>
2026-02-22 11:13:39 +0100

    Modifica dei titoli del login e signup

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -7,7 +7,7 @@ function LoginForm() {
 
     return (
         <>
-            <h1>Sezione del login</h1>
+            <h1 className="text-3xl font-bold">Accesso</h1>
             <div className="flex flex-horizontal gap-2">
                 <p>Non hai un account?</p>
                 <a href="#" role="button" className="link link-primary" onClick={(e) => { e.preventDefault(); setAuthMode(1); }}>Registrati</a>
--- frontend/src/components/SignupForm.jsx
+++ frontend/src/components/SignupForm.jsx
@@ -7,7 +7,7 @@ function SignupForm() {
 
     return (
         <>
-            <h1>Sezione della registrazione</h1>
+            <h1 className="text-3xl font-bold">Registrazione</h1>
             <div className="flex flex-horizontal gap-2">
                 <p>Hai già un account?</p>
                 <a href="#" role="button" className="link link-primary" onClick={(e) => { e.preventDefault(); setAuthMode(0); }}>Accedi</a>
