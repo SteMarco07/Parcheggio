09ce8a168d2aebe4b800bb2dc8e611545cd8fc35
PaoloRobolini <nonilmiopsn@gmail.com>
2026-02-27 08:26:35 +0100

    Update PaginaPrenotazioni.jsx

--- frontend/src/pages/PaginaPrenotazioni.jsx
+++ frontend/src/pages/PaginaPrenotazioni.jsx
@@ -9,11 +9,13 @@ function PaginaPrenotazioni() {
       <h2 className="text-2xl font-semibold mb-4">Prenotazioni</h2>
       { prenotazioni.length === 0 ? 
         (<p>Qui verranno mostrate le prenotazioni future.</p>) : (
-          prenotazioni.map(
-            (prenotazione) => (
+          <div className = "grid grid-cols-3 gap-4">
+            {
+            prenotazioni.map((prenotazione) => (
               <PrenotazioneCard key={prenotazione.id} prenotazione={prenotazione} />
-            )
-          )
+            ))
+            }
+          </div>
         )}
     </div>
   )
