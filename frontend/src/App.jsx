584dec00aa0879d21ba723bf3d45354d105be203
Fabios07 <fabiosgamingsg@gmail.com>
2026-03-05 19:53:39 +0100

    Creazione PaginaAcquistoPrenotazione
    creata la pagina per acquistare prenotazioni (il tuo carrello), per ora non contiene niente ma successivamente conterrà tutti i parcheggi selezionati dalla pagina parcheggi per acquistare (una sorta di riempilogo)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import Navbar from './components/Navbar.jsx';
 import PaginaParcheggi from './pages/PaginaParcheggi.jsx';
 import PaginaPrenotazioni from './pages/PaginaPrenotazioni.jsx';
 import PaginaAutenticazione from './pages/PaginaAutenticazione.jsx';
+import PaginaAcquistoPrenotazione from './pages/PaginaAcquistoPrenotazione.jsx';
 
 function App() {
   const { loadFromLocalStorage, fetchParcheggi, fetchPrenotazioni } = useStore();
@@ -42,6 +43,15 @@ function App() {
             <>
               <Navbar />
               <PaginaPrenotazioni />
+            </>
+          }
+          />
+
+          {/* Rotta verso la pagina per acquistare prenotazioni */}
+          <Route path="/prenotazione" element={
+            <>
+              <Navbar />
+              <PaginaAcquistoPrenotazione />
             </>
           }
           />
--- frontend/src/components/parcheggi/ParcheggioPopup.jsx
+++ frontend/src/components/parcheggi/ParcheggioPopup.jsx
@@ -1,10 +1,17 @@
+import { useNavigate } from 'react-router-dom';
+
 function ParcheggioPopup({parcheggio}) {
+    
+    const navigate = useNavigate();
 
     return (
         <div className="w-full">
             <h1 className="text-xl font-bold">{parcheggio.nome}</h1>
             <p>Prezzo orario: €{parcheggio.prezzo_orario}</p>
-        </div>
+            <div className="flex justify-center items-center">
+                <button class="btn btn-neutral" onClick={() => navigate('/prenotazione')}>Seleziona</button>
+            </div>
+            </div>
     )
 
 }
