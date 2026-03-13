0f8e57b1e833e96753478e15c9774fc92ba6afec
Marco Stellino <marcoste2007@gmail.com>
2026-03-13 08:14:41 +0100

    Sistemato scorrimento ElencoParcheggi

--- frontend/src/components/parcheggi/ElencoParcheggi.jsx
+++ frontend/src/components/parcheggi/ElencoParcheggi.jsx
@@ -8,19 +8,21 @@ function ElencoParcheggi() {
 
   return (
     <div className="join border border-black rounded-box join-vertical gap-4 h-full ">
-      <div className="join-item">
+      <div className="join-item h-full flex flex-col">
         <div className="flex items-center gap-4 px-4 py-3">
           <img src={pIcon} alt="P" className="h-10 w-10" />
           <h2 className="text-3xl font-semibold">Parcheggi disponibili</h2>
         </div>
-
-        {parcheggi.map((item) => (
-          <div key={item.id} className="card card-border w-full bg-base-90 shadow-xl mb-4">
-            <div className="card-body">
-              <ParcheggioCard parcheggio={item} />
+        <div className='overflow-y-auto flex-1'>
+          {parcheggi.map((item) => (
+            <div key={item.id} className="card card-border w-full bg-base-90 shadow-xl mb-4">
+              <div className="card-body">
+                <ParcheggioCard parcheggio={item} />
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
+
       </div>
     </div>
   );
