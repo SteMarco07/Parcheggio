9035935af2256424fc686fb55c6d9335ae363211
Marco Stellino <marcoste2007@gmail.com>
2026-03-14 11:32:37 +0100

    Creazione pulsanti modifica ed elimina

--- frontend/src/components/dashboard/RecordParcheggi.jsx
+++ frontend/src/components/dashboard/RecordParcheggi.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 
 function RecordParcheggi({ numero, parcheggio }) {
 
+    const onDelete = () => {
+        alert("Funzione non implementata");
+    }
+
+    const onEdit = () => {
+        alert("Funzione non implementata");
+    }
+
     return (
         <tr>
             <td>{numero}</td>
@@ -10,6 +18,8 @@ function RecordParcheggi({ numero, parcheggio }) {
             <td>{parcheggio.prezzo_orario} €/h</td>
             <td>{parcheggio.lat}</td>
             <td>{parcheggio.lng}</td>
+            <td><button className="btn btn-warning" onClick={onEdit}>Modifica</button></td>
+            <td><button className="btn btn-error" onClick={onDelete}>Elimina</button></td>
         </tr>
     );
 
